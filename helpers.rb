e6f08da8738a0813c94a66b966741667d183f241
Andrew Thorp <andrewpthorp@gmail.com>
2013-02-15 16:09:29 -0500

    added icomoon social nav

--- helpers.rb
+++ helpers.rb
@@ -1,4 +1,11 @@
 module NavigationHelpers
+  def social_navigation(opts={})
+    elems = []
+    elems << content_tag(:li, link_to("&#xe006;", "http://www.twitter.com/andrewpthorp", class: "twitter", target: "_blank"))
+    elems << content_tag(:li, link_to("&#xe004;", "http://www.facebook.com/andrewpthorp", class: "facebook", target: "_blank"))
+    elems << content_tag(:li, link_to("&#xe02b;", "http://www.github.com/andrewpthorp", class: "github", target: "_blank"))
+    content_tag :ul, elems.join, id: "social-nav", class: "group #{opts[:class]}"
+  end
 end
 
 helpers NavigationHelpers
