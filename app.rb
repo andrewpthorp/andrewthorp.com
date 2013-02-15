52996c4d5ccb4747ffa2de4f6166e80bcd396f35
Andrew Thorp <andrewpthorp@gmail.com>
2013-02-15 11:53:09 -0500

    placeholders for the rest of my pages

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require_relative 'initializers' if File.exists? './initializers.rb'
 require_relative 'helpers' if File.exists? './helpers.rb'
 
 get "/" do
-  haml :index, :layout => :layout
+  haml :index, layout: true
+end
+
+get "/about" do
+  haml :about, layout: true
+end
+
+get "/blog" do
+  haml :blog, layout: true
+end
+
+get "/portfolio" do
+  haml :portfolio, layout: true
+end
+
+get "/resume" do
+  haml :resume, layout: true
 end
