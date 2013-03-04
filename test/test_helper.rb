bb57e4fccfe6e360d59a4b594154bcf3f220fb47
Andrew Thorp <andrewpthorp@gmail.com>
2013-03-04 11:13:06 -0600

    moved from turn to minitest-colorize

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 # test_helper.rb
 ENV['RACK_ENV'] = 'test'
 require 'minitest/autorun'
+require 'minitest/colorize'
 require 'mocha/setup'
 require 'rack/test'
 require 'data_mapper'
@@ -40,21 +41,3 @@ end
 DatabaseCleaner.strategy = :transaction
 MiniTest::Unit::TestCase.add_setup_hook { DatabaseCleaner.start }
 MiniTest::Unit::TestCase.add_teardown_hook { DatabaseCleaner.clean }
-
-if ENV["ENABLE_TURN"]
-  require 'turn'
-  Turn.config do |c|
-   # use one of output formats:
-   # :outline  - turn's original case/test outline mode [default]
-   # :progress - indicates progress with progress bar
-   # :dotted   - test/unit's traditional dot-progress mode
-   # :pretty   - new pretty reporter
-   # :marshal  - dump output as YAML (normal run mode only)
-   # :cue      - interactive testing
-   c.format  = :pretty
-   # turn on invoke/execute tracing, enable full backtrace
-   c.trace   = true
-   # use humanized test names (works only with :outline format)
-   # c.natural = true
-  end
-end
