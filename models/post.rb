7fd284a7f338d1762028dfa4c868b05e18fae82b
Andrew Thorp <andrewpthorp@gmail.com>
2013-03-11 12:57:01 -0400

    removed modules that didn't make sense

--- models/post.rb
+++ models/post.rb
@@ -29,13 +29,73 @@ class Post
     set_slug(self.title.to_slug)
   end
 
-  # Scopes
+  # Get all published Posts
   def self.published
     all(published: true)
   end
 
+  # Get all Posts tagged with a string
+  def self.tagged_with(string, options = {})
+    tag = Tag.first(name: string)
+    conditions = {
+      'taggings.tag_id' => tag.kind_of?(Tag) ? tag.id : nil
+    }
+    all(conditions.update(options))
+  end
 
-  # Instance
+  # Query for the total number of pages.
+  def self.pages(per_page = PER_PAGE, opts={})
+    if opts[:published]
+      query = Post.published
+    else
+      query = Post
+    end
+
+    if !opts[:query] || opts[:query] == :all
+      c = query.count
+    elsif opts[:query_args].blank?
+      c = query.send(opts[:query]).count
+    else
+      c = query.send(opts[:query], opts[:query_args]).count
+    end
+
+    return 1 if c < per_page
+    (c.to_f / per_page).ceil
+  end
+
+  # Query for a specific page.
+  def self.page(page = 1, per_page = PER_PAGE)
+    page = 1 if page.nil?
+    page = page.to_i unless page.is_a? Integer
+
+    offset = (page - 1) * per_page
+    all[offset, per_page]
+  end
+
+  # Get tag_list for a specific instance.
+  def tag_list
+    @tag_list ||= tags.map do |tag|
+      tag.name
+    end
+  end
+
+  # Set tag_list on a specific instance.
+  def tag_list=(string)
+    @tag_list = string.to_s.split(',').map { |name| name.strip }.uniq.sort
+    update_tags
+  end
+
+  # Helper for HTML forms.
+  def tag_collection
+    tags.map { |tag| tag.name }.join(', ')
+  end
+
+  # This actually sets the tags prior to saving an instance.
+  def update_tags
+    self.tags = tag_list.map { |name| Tag.first_or_new(name: name) }
+  end
+
+  # Get the Markdown/CodeRay rendered body
   def pretty_body
     rndr = ATMarkdownRenderer.new(filter_html: true, hard_wrap: true)
     options = {
