e879a68e8cc374af11c0e311b6117bcc84d26eec
Andrew Thorp <andrewpthorp@gmail.com>
2013-08-19 10:58:56 -0500

    added tomdoc to navigatiohelper

--- lib/helpers/navigation_helper.rb
+++ lib/helpers/navigation_helper.rb
@@ -1,4 +1,18 @@
+# Public: Everything that helps with navigation lives inside of this module.
+#
+# Examples
+#
+#   class AndrewThorp < Sinatra::Base
+#     helpers NavigationHelper
+#   end
 module NavigationHelper
+
+  # Public: Return an unordered list (ul) of social links.
+  #
+  # opts  - A Hash used to modify the result.
+  #         :class - A String that is passed on and set as the class on the ul.
+  #
+  # Returns a String.
   def social_navigation(opts={})
     elems = []
     elems << content_tag(:li, link_to("&#xe006;", "http://www.twitter.com/andrewpthorp", class: "twitter", target: "_blank"))
@@ -7,6 +21,12 @@ module NavigationHelper
     content_tag :ul, elems.join, id: "social-nav", class: "group #{opts[:class]}"
   end
 
+  # Public: Return the global navigation in an unordered list (ul).
+  #
+  # opts  - A Hash used to modify the result.
+  #         :class - A String that is passed on and set as the class on the ul.
+  #
+  # Returns a String.
   def site_navigation(opts={})
     elems = []
     elems << content_tag(:li, link_to("", "/", class: "icon-home no-underline", title: "Home"))
