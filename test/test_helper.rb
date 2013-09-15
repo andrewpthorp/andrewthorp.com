a00f777e32f23a235c20c33b384ef840a7aab34f
Andrew Thorp <andrewpthorp@gmail.com>
2013-09-15 10:02:55 -0500

    fixed tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,16 +5,14 @@ require 'minitest/colorize'
 require 'minitest/stub_const'
 require 'mocha/setup'
 require 'rack/test'
-require 'data_mapper'
-require 'dm-is-sluggable'
-require 'dm-constraints'
 require 'database_cleaner'
 require 'shoulda/context'
 require 'factory_girl'
-require 'haml'
-require 'sass'
-require "emoji"
 require 'turn/autorun'
+
+require 'data_mapper'
+require 'dm-is-sluggable'
+require 'emoji'
 
 require_relative '../andrewthorp'
 
@@ -40,7 +38,7 @@ class MiniTest::Unit::TestCase
 end
 
 # Require all libraries
-Dir["./lib/*.rb", "./models/*.rb", "./test/*.rb", "./test/factories/*.rb"].each { |f| require f }
+Dir["./lib/*.rb", "./app/models/*.rb", "./test/*.rb", "./test/factories/*.rb"].each { |f| require f }
 
 # Setup sqlite3 database
 DataMapper::setup(:default, "sqlite3::memory:")
